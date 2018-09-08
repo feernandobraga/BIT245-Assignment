76be633a1d1e0dc329801123a672ed631a5f276b
Fernando <fernando.bragga@gmail.com>
2018-09-08 17:46:27 +1000

    #2

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="scripts.js"></script>
+  <title>Fernando Braga</title>
+</head>
+<body>
+<div class="centerviewpoint">
+
+  <header>
+
+  </header>
+
+  <main>
+
+
+  </main>
+
+  <footer>
+    <p> Fernando Braga Photography © | fernando.bragga@gmail.com </p>
+  </footer>
+
+</div>
+
+</body>
+</html>
